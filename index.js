1f89d6fa4681bc08a036a85de7304ff88e920e7c
Dipra Das <dipradas5940@gmail.com>
2022-11-20 13:30:22 +0600

    74.6

--- index.js
+++ index.js
@@ -18,14 +18,24 @@ async function run() {
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
 
         app.get('/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            console.log(date);
             const query = {};
             const options = await appointmentOptionCollection.find(query).toArray();
+            const bookingQuery = {appointmentDate: date}
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+            options.forEach(option => {
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+                const bookedSlots = optionBooked.map(book => book.slot);
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot));
+                option.slots = remainingSlots;
+                console.log(option.name, bookedSlots, remainingSlots.length);
+            })
             res.send(options);
         });
 
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
-            console.log(booking);
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
 
