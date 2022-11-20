0580c9912eae480407000d577c2d0ef67f0cb2fd
Dipra Das <dipradas5940@gmail.com>
2022-11-21 00:25:00 +0600

    75.4

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
+const jwt = require('jsonwebtoken');
 
 app.use(cors());
 app.use(express.json());
@@ -53,6 +54,17 @@ async function run() {
             }
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
+        })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'});
+                return res.send({accessToken: token});
+            }
+            res.status(403).send({ accessToken: '' })
         })
 
         app.post('/users', async (req, res) => {
