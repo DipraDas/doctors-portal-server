c799722b779a24e915956c38d65a7b4f55ec7c1b
Dipra Das <dipradas5940@gmail.com>
2022-11-20 11:21:01 +0600

    74.2

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.slxro.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
+        app.get('/appointmentOptions', async (req, res) => {
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+            res.send(options);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
 app.get('/', (req, res) => {
     res.send('Server Running');
 })
 
-app.listen(port, () => 
-    console.log(`{port}`)
+app.listen(port, () =>
+    console.log({ port })
 )
